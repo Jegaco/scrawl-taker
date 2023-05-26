0ac9c625260256d0021fc77d9ad5a7ff88ce7560
Jegaco <jeffgcorpuz@gmail.com>
2023-05-25 20:49:17 -0700

    GET and POST routes working

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # scrawl-taker
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+Note taking tool using Express.js
+
+## Description
+Appication that allows users to create, save and delete notes using Express and Node.js
+
+## Screenshots
+
+
+## Deployed Application Links:
+
+
+## Built With
+- Heroku:[ Heroku ](https://www.heroku.com/)
+- Node.js [Version 18.15.0](https://nodejs.org/dist/latest-v18.x/docs/api/)
+- Express.js:[Express.js](https://expressjs.com/en/starter/installing.html)
---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+// Route returns the index.html file
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html')
+));
+
+// Route for notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// Route for stored notes data as JSON
+app.get('/api/notes', async (req, res) => {
+  const parseDb = await JSON.parse(fs.readFileSync("db/db.json", "utf8"));
+  res.json(parseDb);
+});
+
+// Route for new notes, pushes 
+app.post('/api/notes', (req,res) => {
+  const parseDb = JSON.parse(fs.readFileSync("db/db.json", "utf8"))
+  const newNote = {
+    title: req.body.title,
+    text: req.body.text,
+    id: uuidv4(),
+  };
+
+  parseDb.push(newNote);
+  fs.writeFileSync("db/db.json", JSON.stringify(parseDb));
+  res.json(parseDb);
+});
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
